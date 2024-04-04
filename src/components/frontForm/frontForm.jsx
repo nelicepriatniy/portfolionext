38d5bd082a1d5f8cda96eaaf343df4f6a5cbae56
Nelicepriatniy <clavik.polyakov@yabdex.ru>
2024-04-05 01:35:38 +0300

    form Added

--- src/components/frontForm/frontForm.jsx
+++ src/components/frontForm/frontForm.jsx
@@ -17,35 +17,35 @@ let FrontForm = ()=>{
         setPhoneValue(e.target.value)
     }
 
-    const submitForm = async ()=>{
-        if(nameValue != '' && phoneValue != '') {
-            const res = await fetch('http://localhost:3000/api/listPeople',
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    name: nameValue,
-                    phone: phoneValue,
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-            )
-            setNameValue('');
-            setPhoneValue('');
-            alert('Ваша заявка успешно отправлена')
+    // const submitForm = async ()=>{
+    //     if(nameValue != '' && phoneValue != '') {
+    //         const res = await fetch('http://localhost:3000/api/listPeople',
+    //         {
+    //             method: 'POST',
+    //             body: JSON.stringify({
+    //                 name: nameValue,
+    //                 phone: phoneValue,
+    //             }),
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         }
+    //         )
+    //         setNameValue('');
+    //         setPhoneValue('');
+    //         alert('Ваша заявка успешно отправлена')
 
-        } else {
-            alert('заполните все поля')
-        }
-    }
+    //     } else {
+    //         alert('заполните все поля')
+    //     }
+    // }
     
     return (
         <>
             <div className={s.form}>
                 <input type="text" value={nameValue} onChange={nameValueChange} className={s.input} placeholder="Как к Вам обращаться?" />
                 <input type="text" value={phoneValue} onChange={phoneValueChange} className={s.input} placeholder="Как с Вами связаться? " />
-                <button className={s.submit} onClick={submitForm}>Отправить</button>
+                {/* <button className={s.submit} onClick={submitForm}>Отправить</button> */}
             </div>
         </>
     );
