16424cf02479676fe7dca4a59e86e2f05bb323f2
Nelicepriatniy <clavik.polyakov@yabdex.ru>
2024-04-05 03:20:09 +0300

    delete form block

--- src/components/sponsor/sponsor.jsx
+++ src/components/sponsor/sponsor.jsx
@@ -7,7 +7,6 @@ function Sponsor(props) {
     <div className={s.sponsorBlock}>
       <p className={s.sponsorText}>
         Вы можете связаться со мной в <span>соц. сетях</span> , указанных на сайте, я отвечу в <span>течении 30 минут</span>  с пн. по пт. <span>с 12:00 до 00:00</span> по мск <br /><br />
-        Или заполнить форму обратной связи:
       </p>
                 {/* <FrontForm /> */}
       <p className={s.dolg}>
