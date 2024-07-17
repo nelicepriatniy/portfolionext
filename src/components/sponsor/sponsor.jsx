4dcab1b7ba2437f05b570b47a73a36e217f53e50
Nelicepriatniy <clavik.polyakov@yabdex.ru>
2024-07-17 23:10:33 +0300

    add new keys

--- src/components/sponsor/sponsor.jsx
+++ src/components/sponsor/sponsor.jsx
@@ -1,4 +1,3 @@
-import FrontForm from '../frontForm/frontForm';
 import s from './sponsor.module.scss'
 
 function Sponsor(props) {
@@ -8,7 +7,6 @@ function Sponsor(props) {
       <p className={s.sponsorText}>
         Вы можете связаться со мной в <span>соц. сетях</span> , указанных на сайте, я отвечу в <span>течении 30 минут</span>  с пн. по пт. <span>с 12:00 до 00:00</span> по мск <br /><br />
       </p>
-                {/* <FrontForm /> */}
       <p className={s.dolg}>
         Санин долг = 
         {' ' + props.dolg}
